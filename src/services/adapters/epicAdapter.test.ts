05357931575a09c7948fb63547868331e97ea05e
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2022-12-24 02:40:23 +0700

    test: epicAdapter

--- src/services/adapters/epicAdapter.test.ts
+++ src/services/adapters/epicAdapter.test.ts
@@ -1,12 +1,32 @@
-import { describe, test } from 'vitest';
+import type { TEpic } from '@/db/scheme';
+import { describe, expect, test } from 'vitest';
+import epicAdapter from './epicAdapter';
+
+function createTestEpic(estimate: number): TEpic {
+  return {
+    id: 1,
+    title: '',
+    estimate,
+  };
+}
 
 describe('Unit: epicAdapter', () => {
   describe('estimate', () => {
-    test.todo('Если estimate = 0, то estimate должен стать 0h');
-    test.todo('Если estimate = 4, то estimate должен стать 4h');
-    test.todo('Если estimate = 10, то estimate должен стать 1d 2h');
-    test.todo('Если estimate = 44, то estimate должен стать 1w 4h');
-    test.todo('Если estimate = 56, то estimate должен стать 1w 2d');
-    test.todo('Если estimate = 58, то estimate должен стать 1w 2d 2h');
+    test.each([
+      { est: 0, res: '0h' },
+      { est: 4, res: '4h' },
+      { est: 8, res: '1d' },
+      { est: 10, res: '1d 2h' },
+      { est: 44, res: '1w 4h' },
+      { est: 56, res: '1w 2d' },
+      { est: 58, res: '1w 2d 2h' },
+    ])(
+      'Если estimate = $est, то estimate должен стать $res',
+      ({ est, res }) => {
+        const testEpic = createTestEpic(est);
+        const adaptedEpic = epicAdapter(testEpic);
+        expect(adaptedEpic.estimate).toBe(res);
+      }
+    );
   });
 });
