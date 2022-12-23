ec95a76ccbdb3bc3e1deec21127db0ce38f8eb38
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2022-12-24 02:40:55 +0700

    fix: epicAdapter after test

--- src/services/adapters/epicAdapter.ts
+++ src/services/adapters/epicAdapter.ts
@@ -13,21 +13,24 @@ export default function epicAdapter(epic: TEpic): TEpicClient {
   let days: number = 0;
   let weeks: number = 0;
 
-  if (estimate > HOURS_IN_DAY) {
+  if (estimate >= HOURS_IN_DAY) {
     days = Math.floor(estimate / HOURS_IN_DAY);
     hours = estimate % HOURS_IN_DAY;
   } else {
     hours = estimate;
   }
 
-  if (days && days > DAYS_IN_WEEK) {
+  if (days && days >= DAYS_IN_WEEK) {
     weeks = Math.floor(days / DAYS_IN_WEEK);
     days = days % DAYS_IN_WEEK;
   }
 
-  let estimateStr = `${hours}h`;
-  days && (estimateStr = `${days}d ${estimateStr}`);
-  weeks && (estimateStr = `${weeks}w ${estimateStr}`);
+  const estimateArr = [];
+  weeks && estimateArr.push(`${weeks}w`);
+  days && estimateArr.push(`${days}d`);
+  hours && estimateArr.push(`${hours}h`);
+
+  const estimateStr = estimateArr.join(' ') || '0h';
 
   return {
     ...epic,
