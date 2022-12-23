6863954dc856ac3d34fd90cdca92a4fe696c828a
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2022-12-24 01:59:11 +0700

    test: OwnerLink

--- src/components/epic/OwnerLink.test.ts
+++ src/components/epic/OwnerLink.test.ts
@@ -1,6 +1,24 @@
-import { describe, test } from 'vitest';
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import OwnerLink from './OwnerLink.vue';
 
-describe('Unit: OwnerLink', () => {
-  test.todo('Если передан link, должен отрендерить ссылку');
-  test.todo('Если не передан link, ссылки не должно быть');
+function createOwnerLink(link = '') {
+  return mount(OwnerLink, {
+    props: {
+      link,
+    },
+  });
+}
+
+describe.concurrent('Unit: OwnerLink', () => {
+  test('Если передан link, должен отрендерить ссылку', () => {
+    const wrapper = createOwnerLink('http://');
+    const link = wrapper.find('a');
+    expect(link.exists()).toBeTruthy();
+    expect(link.attributes('href')).toBe('http://');
+  });
+  test('Если не передан link, ссылки не должно быть', () => {
+    const wrapper = createOwnerLink();
+    expect(wrapper.find('a').exists()).toBeFalsy();
+  });
 });
